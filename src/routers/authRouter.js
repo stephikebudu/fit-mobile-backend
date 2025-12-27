d610864f823548aa47381f37a58268d9b4ce2bcd
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-27 17:12:23 +0100

    feat: build identifier middleware for signout, send & verify code apis

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const authController = require("../../src/controllers/authController");
+const { identifier } = require("../../src/middleware/identification");
 
 const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/signin", authController.signin);
-router.post("/signout", authController.signout);
+router.post("/signout", identifier, authController.signout);
 
-router.patch("/verify-reset-code", authController.sendVerificationCode);
+router.patch("/send-verification-code", identifier, authController.sendVerificationCode);
+router.patch("/verify-verification-code", identifier, authController.verifyVerificationCode);
+
 module.exports = router;
