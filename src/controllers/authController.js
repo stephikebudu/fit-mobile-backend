d07f28f043dea0011f6e78ce69f5ea06fd339f8d
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-29 17:03:23 +0100

    feat: build send forgot pwd logic, build verify pwd logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,7 +27,7 @@ exports.signup = async (req, res) => {
     res.status(201).json({
       success: true,
       message: "Your account has been successfully created",
-      data,
+      data: data,
     })
   } catch (error) {
     console.error("Could not register user ===", error.message);
@@ -145,14 +145,14 @@ exports.verifyVerificationCode = async (req, res) => {
     }
 
     const codeValue = providedCode.toString();
-    const existingUser = await User.findOne({ email }).select("+verificationCode+verificationCodeValidation");
+    const existingUser = await User.findOne({ email }).select("+verificationCode +verificationCodeValidation");
 
-    if (!existingUser.verified) {
+    if (!existingUser) {
       return res.status(401)
                 .json({ success: false, message: "User does not exist!" })
     }
 
-    if (existingUser) {
+    if (existingUser.verified) {
       return res.status(400).json({ success: false, message: "User already verified!" });
     }
 
@@ -222,5 +222,88 @@ exports.changePassword = async (req, res) => {
       .json({ success: true, message: "Password updated!" })
   } catch (error) {
     console.log(error)
+  }
+}
+
+exports.sendForgotPasswordCode = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({
+          success: false,
+          message: "User does not exist!"
+        });
+    }
+
+    const codeValue = (Math.floor(Math.random() * 10000)).toString();
+
+    let info = await transport.sendMail({
+      from: process.env.NODE_VERIFICATION_CODE_EMAIL,
+      to: existingUser.email,
+      subject: "Fit Mobile: Forgot Password Code",
+      html: "<h1>" + codeValue + "</h1>"
+    });
+
+    if (info.accepted[0] === existingUser.email) {
+      const hashedCodeValue = hmacProcess(codeValue, process.env.HMAC_VERIFICATION_CODE_SECRET)
+      existingUser.forgotPasswordCode = hashedCodeValue;
+      existingUser.forgotPasswordCodeValidation = Date.now();
+      await existingUser.save();
+      return res.status(200).json({ success: true, message: "Forgot password code sent!" })
+    }
+    res.status(400).json({ success: false, message: "Code not sent, try again."})
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.verifyForgotPasswordCode = async (req, res) => {
+  const { email, providedCode, newPassword } = req.body;
+  try {
+    const { error, value } = acceptFPCodeSchema.validate({ email, providedCode, newPassword });
+    if (error) {
+      return res.status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
+
+    const codeValue = providedCode.toString();
+    const existingUser = await User.findOne({ email }).select("+forgotPasswordCode +forgotPasswordCodeValidation");
+
+    if (!existingUser) {
+      return res.status(401)
+                .json({ success: false, message: "User does not exist!" })
+    }
+
+    if (!existingUser.verificationCode || !existingUser.verificationCodeValidation) {
+      return res.status(400)
+        .json({ success: false, message: "Something is wrong with verification code!" });
+    }
+
+    if (Date.now() - existingUser.verificationCodeValidation > 5 * 60 * 1000) {
+      return res.status(400)
+        .json({ success: false, message: "Code has expired!" });
+    }
+
+    const hashedCodeValue = hmacProcess(codeValue, process.env.HMAC_VERIFICATION_CODE_SECRET)
+
+    if (hashedCodeValue === existingUser.verificationCode) {
+      existingUser.verified = true;
+      existingUser.verificationCode = undefined;
+      existingUser.verificationCodeValidation = undefined;
+      await existingUser.save();
+
+      return res.status(200)
+        .json({ success: true, message: "User account has been verified!" })
+    }
+
+    return res.status(400).json({ success: false, message: "Unexpected occurred!" });
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ success: false, message: "The worst case scenario has literally happened :/" })
   }
 }
