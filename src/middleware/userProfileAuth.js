34dd66ccf7f10fa514dbd5e0d6a3b0b279a45e18
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-29 19:37:18 +0100

    lib: dev user profile auth middleware

---
+++ src/middleware/userProfileAuth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const userProfileAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "Cross-check Header for authorization key and value"
+    })
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = { id: payload.userId };
+    next();
+  } catch (error) {
+    console.log(error)
+    res.status(401).json({
+      success: false,
+      message: "Authorization failure: User not authorized!"
+    })
+  }
+}
+
+module.exports = userProfileAuth;
