eee60b15f0e8e3de43dfe593fc449b44b5b03434
Stephany Ikebudu <stephikebudu@gmail.com>
2026-01-05 10:57:53 +0100

    lib: dev validation logic for user profile update, user profile update auth

--- src/middleware/userProfileAuth.js
+++ src/middleware/userProfileAuth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const { updateUserProfileSchema } = require("../../src/middleware/validator.js");
 
-const userProfileAuth = async (req, res, next) => {
+exports.userProfileAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({
@@ -19,8 +20,40 @@ const userProfileAuth = async (req, res, next) => {
     res.status(401).json({
       success: false,
       message: "Authorization failure: User not authorized!"
-    })
+    });
   }
 }
 
-module.exports = userProfileAuth;
+exports.validateUserProfileUpdate = (req, res, next) => {
+  const { error } = updateUserProfileSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.details[0].message
+    });
+  }
+  next();
+}
+
+exports.validateUserProfileUpdateAuth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "No token provided",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = { id: payload.id || payload.userId };
+    next();
+  } catch (error) {
+    console.error("User Authorization Error:", error);
+    res.status(401).json({
+      success: false,
+      message: "User not authorized"
+    });
+  }
+}
