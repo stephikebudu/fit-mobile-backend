49bf696fc460cea0cb586a1b01cf8ee86d331f84
Stephany Ikebudu <stephikebudu@gmail.com>
2026-01-05 12:30:49 +0100

    feat: add user social links update route to server

--- src/routers/userProfileRouter.js
+++ src/routers/userProfileRouter.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const userProfileController = require("../../src/controllers/userProfileController");
-const { userProfileAuth, validateUserProfileUpdate, validateUserProfileUpdateAuth } = require("../../src/middleware/userProfileAuth");
+const {
+  userProfileAuth,
+  validateUserProfileUpdateAuth,
+  validateUserProfileUpdate,
+  validateUserSocialLinks
+} = require("../../src/middleware/userProfileAuth");
 const router = express.Router();
 
 router.get("/profile", userProfileAuth, userProfileController.getUserProfile);
 router.patch("/profile/update", validateUserProfileUpdateAuth, validateUserProfileUpdate, userProfileController.updateUserProfile);
+router.patch("/profile/social-links", validateUserProfileUpdateAuth, validateUserSocialLinks, userProfileController.updateUserSocialLinks);
 
 module.exports = router;
