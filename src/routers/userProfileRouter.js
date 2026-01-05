7c474949fb0d5d40ac9637ba3cb1acd92a80574e
Stephany Ikebudu <stephikebudu@gmail.com>
2026-01-05 10:59:53 +0100

    feat: add server route for user profile update

--- src/routers/userProfileRouter.js
+++ src/routers/userProfileRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userProfileController = require("../../src/controllers/userProfileController");
-const userProfileAuth = require("../../src/middleware/userProfileAuth");
+const { userProfileAuth, validateUserProfileUpdate, validateUserProfileUpdateAuth } = require("../../src/middleware/userProfileAuth");
 const router = express.Router();
 
-router.get("/profile", userProfileAuth, userProfileController.getUserProfile)
+router.get("/profile", userProfileAuth, userProfileController.getUserProfile);
+router.patch("/profile/update", validateUserProfileUpdateAuth, validateUserProfileUpdate, userProfileController.updateUserProfile);
 
 module.exports = router;
