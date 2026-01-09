b34ee7adca940d2e6c309d985b28d31945022e68
Stephany Ikebudu <stephikebudu@gmail.com>
2026-01-09 17:07:46 +0100

    feat: add save user activities route to server

--- src/routers/onboardingRouter.js
+++ src/routers/onboardingRouter.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const onboardingController = require("../../src/controllers/onboardingController");
+const { validateUserProfileUpdateAuth } = require("../../src/middleware/userProfileAuth");
+const { validateSaveUserActivities } = require("../../src/middleware/onboardingAuth");
 
 router.get("/", onboardingController.getActivitiesList);
+router.patch("/", validateUserProfileUpdateAuth, validateSaveUserActivities, onboardingController.saveUserActivities);
 
 module.exports = router;
