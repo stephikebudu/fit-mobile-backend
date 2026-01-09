72843bee5faa8d8e21e502d3a37927c14a70ccd5
Stephany Ikebudu <stephikebudu@gmail.com>
2026-01-09 13:57:45 +0100

    lib: dev user address middleware

--- src/middleware/userProfileAuth.js
+++ src/middleware/userProfileAuth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { updateUserProfileSchema, updateSocialLinksSchema } = require("../../src/middleware/validator.js");
+const { updateUserProfileSchema, updateSocialLinksSchema, updateUserAddressSchema } = require("../../src/middleware/validator.js");
 
 exports.userProfileAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -73,6 +73,20 @@ exports.validateUserSocialLinks = (req, res, next) => {
     return res.status(400).json({
       success: false,
       message: error.details[0].message
+    });
+  }
+  next();
+}
+
+exports.validateUserAddressUpdate = (req, res, next) => {
+  const { error } = updateUserAddressSchema.validate(req.body);
+  if (error) {
+    const messageInfo = error.details[0].type === "object.xor"
+      ? "Provide either structured address in different fields OR a single address string, not both."
+      : error.details[0].message;
+    return res.status(400).json({
+      success: false,
+      message: messageInfo
     });
   }
   next();
