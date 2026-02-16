6a0a1cffe00f832fe72d75b7f2c88b549fb2e178
Stephany Ikebudu <stephikebudu@gmail.com>
2026-02-16 16:17:12 +0100

    feat: add get challenge leaderboard route to server

--- src/routers/challengeRouter.js
+++ src/routers/challengeRouter.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const challengeController = require("../../src/controllers/challengeController");
 const { validateUserProfileUpdateAuth } = require("../../src/middleware/userProfileAuth");
+// const { leaderboardSchema, validateLeaderboardSchema } = require("../../src/middleware/validator");
 
 router.get("/", validateUserProfileUpdateAuth, challengeController.getChallengesList);
 router.get("/:challengeId", validateUserProfileUpdateAuth, challengeController.getChallengeDetails);
 router.post("/:challengeId/join", validateUserProfileUpdateAuth, challengeController.joinChallenge);
+router.get("/:challengeId/leaderboard", validateUserProfileUpdateAuth, challengeController.getChallengeLeaderboard);
 
 module.exports = router;
