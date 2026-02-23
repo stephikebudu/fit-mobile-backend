11eea443ddc42783da3650cbb5da5a810c769d0f
Stephany Ikebudu <stephikebudu@gmail.com>
2026-02-23 12:10:07 +0100

    init payment methods base route in server

--- index.js
+++ index.js
@@ -28,12 +28,14 @@ const userProfileRouter = require("./src/routers/userProfileRouter");
 const onboardingRouter = require("./src/routers/onboardingRouter");
 const challengeRouter = require("./src/routers/challengeRouter");
 const productRouter = require("./src/routers/productRouter");
+const paymentMethod = require("./src/routers/paymentMethodRouter");
 
 app.use("/api/auth", authRouter);
 app.use("/api/user-profile", userProfileRouter);
 app.use("/api/activities", onboardingRouter);
 app.use("/api/challenges", challengeRouter);
 app.use("/api/products", productRouter);
+app.use("/api/payment-methods", paymentMethod);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Database connected");
